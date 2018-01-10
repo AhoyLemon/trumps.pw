bc70530c3006cc0d214f287e6aec5f29937c5cf0
Lemon <AhoyLemon@users.noreply.github.com>
2018-01-10 11:37:13 -0600

    Adjusting close button

--- js/partials/_vue.js
+++ js/partials/_vue.js
@@ -31,6 +31,17 @@ var app = new Vue({
     rounds: [],
   },
   methods: {
+    
+    siteByLemon: function() {
+      
+      var onClick = "sendEvent('outside link', 'https://ahoylemon.xyz', 'banner')";
+      
+      new PNotify({
+        hide: false,
+        addclass: 'stack-bottomright site-by-lemon',
+        text: '<div class="inside animated bounceInDown"><div class="icon-holder"><img src="svg/lemon.svg" class="icon"></div><div class="text-holder"><a href="https://ahoylemon.xyz" target="_blank" onclick="'+onClick+'" class="text">This site is by Lemon.</a></div></div>'
+      });
+    },
 
     trumpPicked: function(t) {
       var self = this;
@@ -382,62 +393,15 @@ var app = new Vue({
       
       self.nextRound();
     },
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // CHECK BROWSER
-    /*
-    checkBrowser: function() {
-      var ua = navigator.userAgent.toLowerCase();
-      //console.log(ua);
-      if (ua.indexOf("android") > -1) {
-        this.device = "android";
-        if (ua.indexOf("firefox") > -1) {
-          // Android Firefox
-          this.browser="firefox";
-        } else if (ua.indexOf("opr") > -1) {
-          // Android Opera
-          this.browser="opera";
-        } else if (ua.indexOf("chrome") > -1) {
-          // Android Chrome
-          this.browser="chrome";
-        }
-      } else if (ua.indexOf('iphone') > -1 || ua.indexOf('ipad') > -1 || ua.indexOf('ipod') > -1) {
-        this.device = "ios";
-      } else if (ua.indexOf('windows') > -1) {
-        this.device = "windows";
-        if (ua.indexOf("edge") > -1) {
-          this.browser = "edge";
-        } else if (ua.indexOf("trident") > -1) {
-          this.browser = "ie";
-        } else if (ua.indexOf('firefox') > -1) {
-          this.browser = "firefox";
-        } else if (ua.indexOf('opr') > -1) {
-          this.browser = "opera";
-        } else if (ua.indexOf('vivaldi') > -1) {
-          this.browser = "vivaldi";
-        } else if (ua.indexOf('chrome') > -1) {
-          this.browser = "chrome";
-        }
-      } else if (ua.indexOf('mac') > -1) {
-        this.device = "mac";
-
-        if (ua.indexOf('chrome') > -1) {
-          this.browser = "chrome";
-        } else if (ua.indexOf('safari') > -1) {
-          this.browser = "safari";
-        } else if (ua.indexOf('firefox') > -1) {
-          this.browser = "firefox";
-        }
-      } else if (ua.indexOf('cros') > -1) {
-        this.device = "chrome";
-        this.browser = "chrome";
-      }
-    }
-    */
-
 
   },
   mounted: function () {
-
+    var self = this;
+    
+    setTimeout(function(){ 
+      self.siteByLemon();
+    }, 16000);
+    
   }
 });
 
